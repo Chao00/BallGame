61c3297a326a15c82bf9e561d4fd73d2d75541e4
Chao <nickzhang0608@gmail.com>
2018-09-11 13:57:08 -0700

    Add built project in WebGL

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public Rigidbody rb;
+	public float forwardForce = 2000f;
+	public float sidewayForce = 500f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.AddForce(0,0,forwardForce * Time.deltaTime);
+		if (Input.GetKey("d"))
+		{
+			rb.AddForce(sidewayForce * Time.deltaTime,0,0, ForceMode.VelocityChange);
+		}
+		if (Input.GetKey("a"))
+		{
+			rb.AddForce(-sidewayForce * Time.deltaTime,0,0,ForceMode.VelocityChange);
+		}
+	}
+}
